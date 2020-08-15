36c2a0032320f556177b90dcc3a57f0212f13816
nithish ravindra <nithishravindra8@gmail.com>
2020-08-15 13:29:34 +0530

    updated dropdown icons

--- src/components/Course.js
+++ src/components/Course.js
@@ -11,13 +11,18 @@ import {
 import { CircularProgressbar } from "react-circular-progressbar";
 import "react-circular-progressbar/dist/styles.css";
 import "./course.css";
+// import { Link, Route } from "react-router-dom";
 
 export default class Course extends Component {
   constructor() {
     super();
 
     this.state = {
-      dropdownOpen: false,
+      iconsAssets: [
+        require("../assets/courses/doc.png"),
+        require("../assets/courses/pro.png"),
+        require("../assets/courses/vid.png"),
+      ],
       dislayIcons: false,
       courses: [
         {
@@ -32,7 +37,7 @@ export default class Course extends Component {
               Text: "this is Text Link",
               sample: "THis is Sample Link",
               progressVal: 80,
-              showAssets: true,
+              showAssets: false,
             },
             {
               Title: "This is title",
@@ -117,7 +122,7 @@ export default class Course extends Component {
 
   tog = (ele) => {
     for (let i = 0; i < this.state.courses.length; i++) {
-      console.log(this.state.courses[i]);
+      // console.log(this.state.courses[i]);
       if (ele === this.state.courses[i]) {
         ele.toggle = !ele.toggle;
         this.setState({
@@ -127,6 +132,28 @@ export default class Course extends Component {
       }
     }
   };
+
+  dispIcon = (s, ind) => {
+    console.log("HEllo");
+    for (let i = 0; i < this.state.courses.length; i++) {
+      if (s === this.state.courses[i]) {
+        console.log(this.state.courses[i]);
+        s.SubCat[ind].showAssets = !s.SubCat[ind].showAssets;
+        s.toggle = !s.toggle;
+        this.setState({
+          courses: [...this.state.courses],
+        });
+        break;
+      }
+    }
+  };
+
+  // handleDispIcon = (type) => {
+  //   console.log("handle displ icon");
+  //   let loc = this.props.location.pathname + "/" + type;
+
+  //   return <Route to={loc} />;
+  // };
 
   render() {
     return (
@@ -144,35 +171,59 @@ export default class Course extends Component {
                     <Row xs={3}>
                       {item.SubCat.map((sub, i) => (
                         <Col key={i}>
-                          <DropdownItem onClick={<h5>This Bar is Clicked</h5>}>
-                            <div
-                              className="menu-item"
-                              style={{ width: 100, height: 150 }}
-                            >
-                              <CircularProgressbar
-                                value={sub.progressVal}
-                                text={sub.Title}
-                                styles={{
-                                  text: {
-                                    fill: "#111111",
-                                    fontSize: "15px",
-                                    fontWeight: 700,
-                                  },
-                                }}
-                                strokeWidth={6}
-                              />
-                              {sub.showAssets ? (
-                                <div>
-                                  <div>
-                                    {
-                                      /* 3 ICONS SHOULD BE GOING HERE*/
-                                      //place them here and Style Them
-                                    }
+                          <DropdownItem onClick={() => this.dispIcon(item, i)}>
+                            <div className="align-icons">
+                              <div
+                                className="menu-item"
+                                style={{ width: 100, height: 150 }}
+                              >
+                                <CircularProgressbar
+                                  value={sub.progressVal}
+                                  text={sub.Title}
+                                  styles={{
+                                    text: {
+                                      fill: "#111111",
+                                      fontSize: "15px",
+                                      fontWeight: 700,
+                                    },
+                                  }}
+                                  strokeWidth={6}
+                                />
+                              </div>
+                              <div>
+                                {sub.showAssets ? (
+                                  <div className="disp-icons">
+                                    {/* <Link
+                                      className="img-2"
+                                      to={this.props.location.pathname + "/vid"}
+                                    > */}
+                                    <img
+                                      className="img-2"
+                                      src={require("../assets/courses/vid.png")}
+                                      alt=""
+                                    ></img>
+                                    {/* </Link>
+                                    <Link
+                                      to={this.props.location.pathname + "/doc"}
+                                    > */}
+                                    <img
+                                      src={require("../assets/courses/doc.png")}
+                                      alt=""
+                                    ></img>
+                                    {/* </Link>
+                                    <Link
+                                      className="img-2"
+                                      to={this.props.location.pathname + "/doc"}
+                                    > */}
+                                    <img
+                                      className="img-2"
+                                      src={require("../assets/courses/pro.png")}
+                                      alt=""
+                                    ></img>
+                                    {/* </Link> */}
                                   </div>
-                                </div>
-                              ) : (
-                                ""
-                              )}
+                                ) : null}
+                              </div>
                             </div>
                           </DropdownItem>
                         </Col>
